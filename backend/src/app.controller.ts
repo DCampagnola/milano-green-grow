214fa9de1fb4f94e922e72e5f18d746c1685b583
Davide Campagnola <dcampagnola.dev@gmail.com>
2021-04-15 23:47:40 +0200

    Added task with cron

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,28 +1,40 @@
-import { Controller, Get, Render } from '@nestjs/common';
-import { getConnection } from 'typeorm';
-import { AppService } from './app.service';
+import {Controller, Get, Render} from '@nestjs/common';
+import {getConnection} from 'typeorm';
+import {AppService} from './app.service';
 import CityHallStats from './models/city-hall-stats.entity';
+import {TaskServiceService} from './task-service/task-service.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService,
+                private readonly taskService: TaskServiceService) {
+    }
 
-  @Get('city-hall-stats')
-  public async getCityHallStats() {
-    return await CityHallStats.find({
-      relations: ['cityHall']
-    })
-  }
+    @Get('city-hall-stats')
+    public async getCityHallStats() {
+        return await CityHallStats.find({
+            relations: ['cityHall']
+        })
+    }
 
 
-  @Get('stats')
-  public async getStats() {
-    const result = await getConnection().getRepository(CityHallStats)
-    .createQueryBuilder('cityHall')
-    .addSelect('SUM(cityHall.nStations)', 'sum')
-        .addSelect('MAX(cityHall.maxDistanceFromStation)', 'max')
-        .addSelect('AVG(cityHall.avgDistanceFromStation)', 'avg')
-    .getRawOne();
-    return {nStations: Number(result.sum).toFixed(0), avgDistanceFromStation: Number(result.avg).toFixed(2), maxDistanceFromStation: Number(result.max).toFixed(2)};
-  }
+    @Get('stats')
+    public async getStats() {
+        const result = await getConnection().getRepository(CityHallStats)
+            .createQueryBuilder('cityHall')
+            .addSelect('SUM(cityHall.nStations)', 'sum')
+            .addSelect('MAX(cityHall.maxDistanceFromStation)', 'max')
+            .addSelect('AVG(cityHall.avgDistanceFromStation)', 'avg')
+            .getRawOne();
+        return {
+            nStations: Number(result.sum).toFixed(0),
+            avgDistanceFromStation: Number(result.avg).toFixed(2),
+            maxDistanceFromStation: Number(result.max).toFixed(2)
+        };
+    }
+
+    @Get('run-task')
+    public async runTask() {
+        await this.taskService.syncService();
+    }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,30 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {HttpModule, Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ScheduleModule} from '@nestjs/schedule';
+import { TaskServiceService } from './task-service/task-service.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-    type: (process.env.DB_TYPE as 'sqlite' | 'mysql') || 'sqlite',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || 'db',
-    entities: ["dist/**/*.entity{.ts,.js}"],
-    synchronize: true,
-    logging: true,
-  }),
-  ConfigModule.forRoot()
-],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: (process.env.DB_TYPE as 'sqlite' | 'mysql') || 'sqlite',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE || 'db',
+            entities: ['dist/**/*.entity{.ts,.js}'],
+            synchronize: true,
+            logging: true,
+        }),
+        ConfigModule.forRoot(),
+        ScheduleModule.forRoot(),
+        HttpModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService, TaskServiceService],
 })
-export class AppModule {}
+export class AppModule {
+}
